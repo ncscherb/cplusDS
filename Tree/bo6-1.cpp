ecab5347e2a5146aaabba093a0d5564706f2d432
ncscherb <dengsenling@126.com>
2018-02-24 09:57:43 +0800

    add seq BiTree

--- Tree/bo6-1.cpp
+++ Tree/bo6-1.cpp
@@ -1,3 +1,5 @@
+//bo6-1.cpp 二叉树的顺序存储（存储结构由c6-1.h定义）的基本操作（23个）
+#define ClearBiTree InitBiTree //在顺序结构中，两个函数完全一样
 //#include "c6-1.h"
 
 //构造空二叉树T,因为T是数组名，故不需要&
@@ -45,6 +47,34 @@ void CreateBiTree(SqBiTree T){
     }
 }
 
+void CreateBiTreeRead(SqBiTree T,string filename) {
+    int i = 0;
+    InitBiTree(T); //构造空二叉树
+
+    ifstream inFile{filename};
+    if (!inFile.is_open()) {
+        printf("Failed to open file: %s\n", filename.c_str());
+        exit(ERROR);
+    }
+
+    while (TRUE) {
+        inFile >> T[i]; //read a value from the file
+        if (inFile.eof()) break;
+        //cout << T[i] << endl;
+        i++;
+    }
+
+    for(i=1;i<MAX_TREE_SIZE;i++){
+        //printf("%d,%d\n",i,(i+1)/2-1);
+        //如果一个节点不是根节点，判断它是否有双亲（根据满二叉树原理）
+        if(i!=0&&T[(i+1)/2-1]==Nil&&T[i]!=Nil){
+            cout<<"出现无双亲的非根节点"<<T[i]<<endl;
+            exit(ERROR);
+        }
+    }
+
+}
+
 //初始条件：二叉树T存在。
 //操作结果：若T为空二叉树，则返回T
 Status BiTreeEmpty(SqBiTree T){
@@ -74,7 +104,7 @@ int BiTreeDepth(SqBiTree T){
 
 //当T不空，用e返回T的根，并返回OK
 Status Root(SqBiTree T, ElemType &e){
-    if(BiTreeDepth(T))
+    if(BiTreeEmpty(T))
         return ERROR;
     else{
         e=T[0];
@@ -209,12 +239,120 @@ void InsertChild(SqBiTree T, ElemType p, int LR, SqBiTree c){
 
     j=2*i+1+LR; //LR=0插入c为p节点的左子树
 
-    if(T[j]!=Nil){ //如果p原来的左子树不为空（如果要插入到左子树的话）
-         //把从T的j节点开始的子树移为从j节点右节点开始的子树(?会不会死循环)
-        Move(T,j,T,2*j+2);
-    }
+//    if(T[j]!=Nil){ //如果p原来的左子树不为空（如果要插入到左子树的话）
+//         //把从T的j节点开始的子树移为从j节点右节点开始的子树(?会不会死循环)
+//        Move(T,j,T,2*j+2);
+//    }
+//    Move(c,k,T,j);
+    if(T[j]!=Nil)
+        Move(T,j,c,2*k+2);
 
     Move(c,k,T,j);
 }
 
+#include "../stack&queue/c3-2.h"
+#include "../stack&queue/bo3-2.cpp"
+//根据LR为1或0，删除T中p所指节点的左或右子树
+//p指向T中某个节点，LR为1或0
+Status DeleteChild(SqBiTree T,position p,int LR){
+    int i;
+    Status k=OK;
+    LinkQueue q;
+    InitQueue(q);
 
+    i=(int)pow(2,p.level-1)+ p.order-2; //找到节点p
+    if(T[i]==Nil)
+        return ERROR;
+    i=2*i+1+LR; //待删除子树的根节点在矩阵中的序号
+    while (k){
+        if(T[2*i+1]!=Nil) //左节点不空
+            EnQueue(q,2*i+1);//入队左节点的序号
+        if(T[2*i+2]!=Nil)
+            EnQueue(q,2*i+2);
+        T[i]=Nil; //删除此节点
+        k=DeQueue(q,i);//队列不空
+    }
+    return OK;
+}
+
+void (*VisitFunc)(ElemType); //定义函数变量
+
+void PreTraverse(SqBiTree T, int e){
+    VisitFunc(T[e]);
+    if(T[2*e+1]!=Nil)
+        PreTraverse(T,2*e+1);
+    if(T[2*e+2])
+        PreTraverse(T,2*e+2);
+}
+//先序遍历二叉树
+void PreOrderTraverse(SqBiTree T, void(*vi)(ElemType)){
+    VisitFunc=vi;
+    if(!BiTreeEmpty(T))
+        PreTraverse(T,0);
+    cout<<endl;
+}
+
+//InOrderTraverse()调用
+//T是遍历的节点，e是从哪个节点开始遍历（e是节点序号）
+void InTraverse(SqBiTree T, int e){
+    if(T[2*e+1]!=Nil)
+        InTraverse(T,2*e+1); //左子树不为空
+    VisitFunc(T[e]);
+    if(T[2*e+2]!=Nil)
+        InTraverse(T,2*e+2); //右子树不为空
+
+}
+
+//中序遍历T，对每个节点调用函数vi一次且仅一次
+void InOrderTraverse(SqBiTree T, void(*vi)(ElemType)){
+    VisitFunc=vi;
+    if(!BiTreeEmpty(T))
+        InTraverse(T,0);
+    cout<<endl;
+}
+
+void PostTraverse(SqBiTree T,int e){
+    if(T[2*e+1]!=Nil)
+        PostTraverse(T,2*e+1); //左子树不为空
+    if(T[2*e+2]!=Nil)
+        PostTraverse(T,2*e+2); //右子树不为空
+    VisitFunc(T[e]);
+}
+
+//后序遍历二叉树
+void PostOrderTraverse(SqBiTree T, void(*vi)(ElemType)){
+    VisitFunc=vi;
+    if(!BiTreeEmpty(T))
+        PostTraverse(T,0);
+    cout<<endl;
+
+}
+
+//层序遍历二叉树
+void LevelOrderTraverse(SqBiTree T,void(*vi)(ElemType)){
+    int i=MAX_TREE_SIZE-1,j;
+    while(T[i]==Nil)
+        i--; //找到最后一个非空节点的序号
+    for(j=0;j<=i;j++) //从跟节点起，安层序遍历二叉树
+        if(T[j]!=Nil)
+            vi(T[j]); //只遍历非空的节点
+    cout<<endl;
+}
+
+//逐层、按本层序号输出二叉树
+void Print(SqBiTree T){
+    int j,k;
+    position p;
+    ElemType e;
+    for(j=1;j<=BiTreeDepth(T);j++){
+        cout<<"第"<<j<<"层：";
+        for(k=1;k<=pow(2,j-1);k++){ //第i层的元素个数
+            p.level=j;
+            p.order=k;
+            e=Value(T,p);
+            if(e!=Nil)
+                cout<<k<<":"<<e<<' ';
+        }
+        cout<<endl;
+    }
+}
--- Tree/main6-1.cpp
+++ Tree/main6-1.cpp
@@ -1,6 +1,7 @@
 //main6-1.cpp 检验bo6-1.cpp的主程序，利用条件编译选择数据类型为char或int
 // #define CHAR 1 //字符型
 #define CHAR 0 //整数型
+#define NOTEST 1 //测试中
 #if CHAR
     typedef char ElemType;
     ElemType Nil=' '; //设字符型以空格为空
@@ -10,17 +11,72 @@
 #endif
 
 #include <iostream>
+#include <fstream> //用于file streams
 using namespace std;
 #include "../c1.h"
 #include "c6-1.h"
 #include "bo6-1.cpp"
 
+void visit(ElemType e){
+    cout<<e<<' ';
+}
+
 int main(){
+    Status i;
+    int j;
+    position p;
+    ElemType e;
     SqBiTree T,s;
     InitBiTree(T);
-    CreateBiTree(T);
+#if NOTEST
+    //CreateBiTree(T);
+    CreateBiTreeRead(T,"E:\\Users\\cplusDS\\Tree\\file");
     cout<<"建立二叉树后，数为空？"<<BiTreeEmpty(T)<<"(1:是，0：否)"<<endl;
     cout<<"数的深度："<<BiTreeDepth(T)<<endl;
+    i=Root(T,e);//获取根节点
+    if(i)
+        cout<<"二叉树的根为："<<e<<endl;
+    else
+        cout<<"树空，无根"<<endl;
+    cout<<"层序遍历二叉树："<<endl;
+    LevelOrderTraverse(T,visit);
+    cout<<"中序遍历二叉树："<<endl;
+    InOrderTraverse(T,visit);
+    cout<<"后序遍历二叉树："<<endl;
+    PostOrderTraverse(T,visit);
+    cout<<"请输入待修改节点的层号 本层序号: ";
+    cin>>p.level>>p.order;
+    e=Value(T,p);
+    cout<<"待修改节点的原值为："<<e<<" 请输入新值：";
+    //cin>>e;
+    //Assign(T,p,e);
+    cout<<"先序遍历二叉树:"<<endl;
+    PreOrderTraverse(T,visit);
+    cout<<"节点"<<e<<"的双亲为"<<Parent(T,e)<<",左右孩子分别为";
+    cout<<LeftChild(T,e)<<","<<RightChild(T,e)<<",左右兄弟节点分别为：";
+    cout<<LeftSibling(T,e)<<","<<RightSibling(T,e)<<endl;
+    InitBiTree(s);
+    cout<<"建立右子树为空的的树s:"<<endl;
+    CreateBiTree(s);
+    cout<<"树s插到树T中，请输入树T中树s的双亲节点 s为左（0）或右（1）";
+    cin>>e>>j;
+    InsertChild(T,e,j,s);
+    Print(T);
+    cout<<"删除子树，请输入带删除子树根节点的层号 本层序号 左（0）或右（1）子树";
+    cin>>p.level>>p.order>>j;
+    DeleteChild(T,p,j);
+    Print(T);
+    ClearBiTree(T);
+    cout<<"清除二叉树后，树空否？"<<BiTreeEmpty(T)<<"(1:是 0:否) 树的深度="<<BiTreeDepth(T)<<endl;
+    i=Root(T,e);
+    if(i)
+        cout<<"二叉树的根为： "<<e<<endl;
+    else
+        cout<<"树空，无根"<<endl;
+#else
+    CreateBiTreeRead(T,"E:\\Users\\cplusDS\\Tree\\file");
+#endif
+
 }
 
 
